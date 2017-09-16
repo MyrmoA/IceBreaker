6a12097542eec3b73660653b0a74347b792976e1
yjj781265 <yjj950406@gmail.com>
2017-09-15 23:16:59 -0400

    initial push

---
+++ app/src/main/java/com/example/jayang/icebreaker/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.jayang.icebreaker;
+
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+     Button mlogin;
+    TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mlogin = (Button)findViewById(R.id.login_button);
+        mTextView =(TextView)findViewById(R.id.register_button);
+
+
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(),RegisterPage.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
