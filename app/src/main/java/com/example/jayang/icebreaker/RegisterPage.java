239213a2a1f3a0abfd3e5d266b75ece70965a43b
yjj781265 <yjj950406@gmail.com>
2017-09-15 23:22:32 -0400

    changed test

--- app/src/main/java/com/example/jayang/icebreaker/RegisterPage.java
+++ app/src/main/java/com/example/jayang/icebreaker/RegisterPage.java
@@ -9,6 +9,6 @@ public class RegisterPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        //test
     }
 }
