96812723da12ecec13dde79d69219af1c5308d4e
yjj781265 <yjj950406@gmail.com>
2017-10-03 03:53:58 -0400

    firebase  auth and real time database package implmented, sign in error handling.

--- app/src/main/java/com/example/jayang/icebreaker/Register2.java
+++ app/src/main/java/com/example/jayang/icebreaker/Register2.java
@@ -1,76 +1,181 @@
 package com.example.jayang.icebreaker;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Register2 extends AppCompatActivity {
-    Button mButton ;
+    Button mButton ; // signup button
     Toolbar mToolbar;
+    EditText email,password,cpassword;
+    String  userEmail, userPassword,userCpassword;
+    final String TAG ="IcebreakerApp" ;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
-
-        mButton =  (Button)findViewById(R.id.signup);
-        mToolbar =(Toolbar)findViewById(R.id.toolbar2);
+        mAuth = FirebaseAuth.getInstance();
+ /*Layout Component*/
+        mButton = (Button) findViewById(R.id.signup);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar2);
         mToolbar.setNavigationIcon(R.mipmap.ic_launcher);
+        email = (EditText) findViewById(R.id.email_R);
+        password = (EditText) findViewById(R.id.password_R);
+        cpassword = (EditText) findViewById(R.id.Cpassword_R);
+
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Register2.this);
-                alertDialogBuilder.setTitle("Email Verification");
-                alertDialogBuilder.setIcon(R.mipmap.ic_launcher);
-                alertDialogBuilder.setMessage("Enter email verification code we send to your registered email");
-
-                EditText editText = new EditText(Register2.this);
-
-                alertDialogBuilder.setView(editText);
-
-
-
-
-                alertDialogBuilder
-                        .setCancelable(false)
-                        .setPositiveButton("Confirm",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog,
-                                                        int id) {
-                                        Toast.makeText(getBaseContext(),"Sign up success",Toast.LENGTH_SHORT).show();
-                                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                                        startActivity(intent);
-                                        finish();
-
-                                    }
-                                })
-                        .setNegativeButton("Discard",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog,
-                                                        int id) {
-                                        dialog.cancel();
-                                    }
-                                });
-                // create alert dialog
-                AlertDialog alertDialog = alertDialogBuilder.create();
-                // show it
-                alertDialog.show();
-
+                attemptRegistration();
 
             }
         });
-
-
-
     }
+
+
+
+
+   private void attemptRegistration(){
+       email.setError(null);
+       password.setError(null);
+       cpassword.setError(null);
+
+        /*String*/
+       // Store values at the time of the login attempt.
+       userEmail = email.getText().toString();
+       userPassword =password.getText().toString();
+       userCpassword = cpassword.getText().toString();
+
+       boolean cancel = false;
+       View focusView = null;
+
+       // Check for a valid email address.
+       if (TextUtils.isEmpty(userEmail)) {
+           email.setError(getString(R.string.error_field_required));
+           focusView = email;
+           cancel = true;
+       } else if (!isEmailValid(userEmail)) {
+           email.setError(getString(R.string.error_invalid_email));
+           focusView = email;
+           cancel = true;
+       }
+
+       // Check for a valid password, if the user entered one.
+       if (!TextUtils.isEmpty(userPassword) && userPassword.length()<6) {
+           password.setError(getString(R.string.error_invalid_password));
+           focusView = password;
+           cancel = true;
+       }else if (TextUtils.isEmpty(userPassword)){
+           password.setError(getString(R.string.error_field_required));
+           focusView = password;
+           cancel = true;
+       }else if (TextUtils.isEmpty(userCpassword)){
+           cpassword.setError(getString(R.string.error_field_required));
+           focusView = cpassword;
+           cancel = true;
+       }
+       else if(!TextUtils.isEmpty(userPassword) &&!TextUtils.isEmpty(userCpassword) &&
+               !userPassword.equals(userCpassword)){
+           cpassword.setError("Password does not match");
+           focusView = cpassword;
+           cancel = true;
+       }
+
+
+
+       if (cancel) {
+           // There was an error; don't attempt login and focus the first
+           // form field with an error.
+           focusView.requestFocus();
+       } else {
+           // firebase signup
+           hideKeyboard();
+           createAccount(userEmail,userPassword);
+       }
+   }
+
+    //Firebase auth user signup method createAccount(,);
+    public void createAccount(String email, String password){
+        final ProgressDialog progressDialog = ProgressDialog.show(this,"Icebreaker","Signing-up",true);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.d(TAG, "onComplete: Failed=" + task.getException().getMessage()); //ADD THIS
+
+                            progressDialog.dismiss();
+                            Toast.makeText(Register2.this, task.getException().getMessage(),
+                                    Toast.LENGTH_LONG).show();
+
+                        }else {
+                            progressDialog.dismiss();
+                            Toast.makeText(Register2.this, "Sign-up success",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    //hide softkeyboard
+    private void hideKeyboard(){
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        // You can add more checking logic here.
+        return email.contains("@");
+    }
+
+
+
+
 }
